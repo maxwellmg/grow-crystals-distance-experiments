f50a935592f221034886ecb1eeb53eff5e658dcd
centrattic <pialityagi@gmail.com>
2025-04-01 02:26:50 +0000

    squashing run_exp changes
    n_exp.sh

--- loss_exp.sh
+++ loss_exp.sh
@@ -5,8 +5,8 @@
 
 # Define arrays for parameters
 data_ids=("lattice") #  "circle"
-model_ids=("standard_transformer" "H_transformer" "standard_MLP" "H_MLP")
-splits=(1)  # Modify as needed
+model_ids=("H_transformer" "H_MLP")
+splits=(3)  # Modify as needed
 ns=(1)
 
 # Iterate over all combinations
@@ -14,7 +14,7 @@ for data_id in "${data_ids[@]}"; do
     for model_id in "${model_ids[@]}"; do
         for split in "${splits[@]}"; do
             for n in "${ns[@]}"; do
-                python src/run_exp.py --data_id "$data_id" --model_id "$model_id" --split "$split" --n "$n"
+                python3 src/run_exp.py --data_id "$data_id" --model_id "$model_id" --split "$split" --n "$n"
             done
         done
     done
